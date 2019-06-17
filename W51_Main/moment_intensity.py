1af7a0f3bcd7c179c40b2d81e815d239fe878bc2
joshmachado <46457626+joshmachado@users.noreply.github.com>
2019-06-17 12:03:45 -0600

    Add files via upload

--- W51_Main/moment_intensity.py
+++ W51_Main/moment_intensity.py
@@ -3,12 +3,12 @@ from astropy.utils import data
 from spectral_cube import SpectralCube
 import aplpy
 
-data = file.fits #data cube used to create moment map
+data = 'cubename.fits' #data cube used to create moment map
 cube =  SpectralCube.read(data)
 
 lower = 0.0 #lower velocity bound for spectral slice
 upper = 1.0 #upper velocity bound for spectral slice
-cube_slice = cube.with_spectral_unit(u.km/u.s, velocity_convention='radio').spectralslab(lower*u.km/u.s, upper*u.km/u.s)
+cube_slice = cube.with_spectral_unit(u.km/u.s, velocity_convention='radio').spectral_slab(lower*u.km/u.s, upper*u.km/u.s)
 
 ### MOMENT MAPS ###
 
@@ -27,7 +27,7 @@ f.save(full_name)
 
 #generating spectral slice cube moment map image
 slice_name = 'Slice_Moment_Map.png' #name of output image
-s = aplpy.FITSFigure(moment_0.hdu)
+s = aplpy.FITSFigure(moment_0_slice.hdu)
 s.show_colorscale()
 s.show_colorbar()
 s.save(slice_name)
@@ -35,7 +35,7 @@ s.save(slice_name)
 ### PEAK INTENSITY MAPS ###
 
 full_peak = cube.max(axis=0)
-slice_peak = cube.max(axis=0)
+slice_peak = cube_slice.max(axis=0)
 
 full_peak.hdu
 slice_peak.hdu
@@ -44,7 +44,7 @@ slice_peak.hdu
 fpeak_name = 'Full_Peak.png' #name of output image
 fp = aplpy.FITSFigure(full_peak.hdu)
 fp.show_colorscale()
-f.show_colorbar()
+fp.show_colorbar()
 fp.save(fpeak_name)
 
 #generating slice cube peak intensity image
@@ -52,8 +52,6 @@ speak_name = 'Slice_Peak.png' #name of output image
 sp = aplpy.FITSFigure(slice_peak.hdu)
 sp.show_colorscale()
 sp.show_colorbar()
-fp.save(speak_name)
+sp.save(speak_name)
 
 exit
-
-
